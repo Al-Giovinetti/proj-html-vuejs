afee627d5595d0adb17d49173d348f46b613ea97
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-26 12:50:52 +0200

    Macrostruttura Header e searchbar component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,78 @@
 <script>
-
+import HeaderSearchbar  from "./HeaderSearchbar.vue"
 export default{
     name:"AppHeader",
+
+    components:{
+        HeaderSearchbar,
+    }
 }
 
 </script>
 
 <template>
-    <h1>ciao</h1>
-
+<header>
+    <div class="header-left">
+        <img src="" alt="MaxCoach logo">
+    </div>
+    <nav class="header-right">
+        <div class="element-nav">
+            <h3>home</h3>
+            <i class="fa-solid fa-angle-down"></i>
+        </div>
+        <div class="element-nav">
+            <h3>home</h3>
+            <i class="fa-solid fa-angle-down"></i>
+        </div>
+        <div class="element-nav">
+            <h3>home</h3>
+            <i class="fa-solid fa-angle-down"></i>
+        </div>
+        <div class="element-nav">
+            <h3>home</h3>
+            <i class="fa-solid fa-angle-down"></i>
+        </div>
+        <div class="element-nav">
+            <h3>home</h3>
+            <i class="fa-solid fa-angle-down"></i>
+        </div>
+        <div class="element-nav">
+            <h3>home</h3>
+            <i class="fa-solid fa-angle-down"></i>
+        </div>
+        <div class="element-nav">
+            <span>carrello</span>
+        </div>
+        <div class="element-nav">
+            <span>user</span>
+        </div>
+        <HeaderSearchbar />
+    </nav>
+</header>
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/mixins.scss" as *;
+
+header{
+    @include flex(row,space-between,center);
+    padding: 0 3.5rem
+}
+
+nav.header-right{
+    @include flex(row,center,center);
+
+    .element-nav{
+        padding: 1rem;
+    }
+
+    
+
+    h3{
+        display: inline;
+    }
+    
+    
+}
 
 </style>
---
+++ src/components/HeaderSearchbar.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default{
+    name:"HeaderSearchbar",
+}
+
+</script>
+
+<template>
+    <div>
+        <input type="text">
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
