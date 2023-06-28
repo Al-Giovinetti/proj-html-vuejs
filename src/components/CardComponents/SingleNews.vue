12666bef7bddb7591acb315fd971b86a46014113
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-28 13:06:18 +0200

    Style news components

--- src/components/CardComponents/SingleNews.vue
+++ src/components/CardComponents/SingleNews.vue
@@ -4,22 +4,48 @@
 export default{
     name:"SingleNews",
 
+    props:{
+        news:Object
+    }
+
 }
 
 </script>
 
 <template>
     <div class="card">
-        <img src="" alt="aaaaaa">
-        <span>aaaa</span>
-        <p>aaaa</p>
-        <span>aaaa</span>
-        <span>aaaaa</span>
+        <img :src="news.img" alt="aaaaaa">
+        <span class="color-grey">{{ news.subject }}</span>
+        <p>{{ news.title }}</p>
+        <i class="fa-regular fa-calendar color-grey"></i>
+        <span class="color-grey">{{ news.date }}</span>
+        <i class="fa-regular fa-eye color-grey"></i>
+        <span class="color-grey">{{  news.views}}</span>
     </div>
 
 </template>
 
 <style lang="scss" scoped>
+    div.card{
+        width: calc(100% / 3 - 4rem);
+        margin: 2rem;
+
+        img{
+            width: 100%;
+            border-radius: 10px;
+            padding-bottom: 2rem;
+        }
+
+        p{
+            font-weight: bold;
+            font-size: 1.5rem;
+            padding: 1.5rem 0;
+        }
+
+        i{
+            padding: 0.5rem;
+        }
+    }
 
 
 </style>
--- src/components/SectionComponents/OursNews.vue
+++ src/components/SectionComponents/OursNews.vue
@@ -6,8 +6,35 @@ export default{
     name:"OursNews",
 
     components:{
-        SingleNews,
-        
+        SingleNews,   
+    },
+
+    data(){
+        return{
+            news:[
+                {
+                img:"https://st2.depositphotos.com/1000423/7385/i/600/depositphotos_73854849-stock-photo-office-work-moments.jpg",
+                subject:"BUSINESS",
+                title:"Ten Benefits Of Rentals That May Change Your Perspective",
+                date:" Dec 28, 2020",
+                views:" 2,621 views"
+                },
+                {
+                img:"https://img.freepik.com/premium-photo/young-female-manager-her-workplace-with-laptop-discussing-work-moments-with-her-pregnant-colleague-office-modern-pregnant-office-worker_173815-41222.jpg",
+                subject:"BUSINESS",
+                title:"10 Things Successful Mompreneurs Do Differente",
+                date:" Dec 28, 2020",
+                views:" 2,341 views"
+                },
+                {
+                img:"https://st4.depositphotos.com/2251265/26614/i/600/depositphotos_266146836-stock-photo-working-moments-group-of-young.jpg",
+                subject:"BUSINESS",
+                title:"How Stay Calm from the First Time.",
+                date:" Dec 28, 2020",
+                views:"1,723 views"
+                }
+            ]
+        }
     }
 }
 
@@ -15,10 +42,11 @@ export default{
 
 <template>
     <section>
-        <p>aaaaaaa</p>
-        <h2>aaaaaaaa</h2>
+        <p class="color-grey">ENJOY READING ON MAXCOACH</p>
+        <h2>Latest on <span>Our Blogs</span></h2>
         <div class="container">
-            <SingleNews  v-for="n in 3"/>
+            <SingleNews  v-for="n in news"
+            :news = "n"/>
         </div>
     </section>
 
@@ -27,9 +55,21 @@ export default{
 
 <style lang="scss" scoped>
 @use "../../styles/partials/mixins.scss" as*;
-
+@use "../../styles/partials/variables.scss" as*;
+section>*:not(div.container){
+    text-align: center;
+    padding-top: 3rem
+}
 div.container{
-    @include flex(row,space-around,start)
+    @include flex(row,space-around,start);
+
+}
+
+h2{
+    font-size: 2.5rem;
+    span{
+        color:$in-hover
+    }
 }
 
 
